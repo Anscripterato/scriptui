762d83318d5bfef12314db433b037fbf34f132bd
Anscripterato <3648132274@qq.com>
2025-01-17 03:55:16 +0800

    更新 wind源码.lua

--- wind源码.lua
+++ wind源码.lua
@@ -8,7 +8,7 @@ local WindUI = {
 }
 local RunService = game:GetService("RunService")
 
-local Themes = require("./init")
+local Themes = require("./Components/init")
 local Creator = require("./Creator")
 local New = Creator.New
 local Tween = Creator.Tween
