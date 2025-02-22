5890982543742f826059caa7b58498c461144cca
Anscripterato <3648132274@qq.com>
2025-02-22 21:29:49 +0800

    更新 main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,1 @@
-local an = game:GetService("Players").LocalPlayer
-local group = 35633654
-local grouplink = "https://www.roblox.com/communities/35633654/unnamed#!/about"
-
-if an:IsInGroup(group) then
-print("成功")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Anscripterato/scriptui/refs/heads/main/hello.lua"))()
-else
-game:GetService("ReplicatedStorage").rEvents.rebirthRemote:InvokeServer("rebirthRequest")	
-wait(3)
-game.Players.LocalPlayer:Kick("傻逼东西还想直接绕过白名单是不是，给你号毁了，不谢😂😂😂")
-end
